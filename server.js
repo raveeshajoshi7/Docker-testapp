0b617bfd28ad7da216e874abbed632926d807ce9
Raveesha Joshi <raveeshajoshi7@gmail.com>
2026-06-08 20:15:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const client = new MongoClient(MONGO_URL);
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
+    await client.connect();
     console.log('Connected successfully to server');
 
     const db = client.db("apnacollege-db");
